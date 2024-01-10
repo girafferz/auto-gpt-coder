f323d120a8b35e6623a4b57d29d5c09fed9a8f73
girafferz <noiman2004+git@gmail.com>
2024-01-10 12:34:11 +0900

    dev

--- src/module/sample.ts
+++ src/module/sample.ts
@@ -1,8 +1,20 @@
+import axios, { AxiosInstance } from 'axios';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'https://api.coindesk.com', // Your base URL
+});
+
 export class Sample {
   constructor() {}
 
-  execute() {
-    // TODO BTCの価格を取得する
-    throw new Error('Method not implemented.');
+  async execute() {
+    const response = await axiosInstance.get('/v1/bpi/currentprice/BTC.json');
+
+    if (response.status !== 200) {
+      throw new Error(`Error when getting BTC price: ${response.status}`);
+    }
+
+    // 価格の一週間を推移をlog表示
+    return response.data.bpi.USD.rate_float; // The path might be different based on the API response
   }
 }
